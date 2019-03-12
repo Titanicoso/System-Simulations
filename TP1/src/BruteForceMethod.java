b02d7772190cb03b4d565bc0b578e3985e429548
Agustin Calatayud <acalatayud@itba.edu.ar>
2019-03-12 15:14:32 -0300

    Brute Force returns map containing neighbours

--- TP1/src/BruteForceMethod.java
+++ TP1/src/BruteForceMethod.java
@@ -1,17 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BruteForceMethod {
-	
-	public static void findNeighbours(final Area area, final boolean cont) {
+
+	public static Map<Integer, List<Particle>> findNeighbours(final Area area, final boolean cont) {
+
+		final Map<Integer, List<Particle>> neighbours = new HashMap<>();
+		for (int i = 0; i < area.getParticles().length; i++) {
+			neighbours.put(i, new ArrayList<>());
+		}
+
 		for (final Particle particle1 : area.getParticles()) {
-			System.out.print(particle1.getId() + ": ");
 			for (final Particle particle2 : area.getParticles()) {
 				if (particle1.getId() != particle2.getId()) {
 					if (particle1.distance(particle2, area, cont) < area.getInteractionRatio()) {
-						System.out.print(particle2.getId() + " ");
+						if (!neighbours.get(particle1.getId()).contains(particle2))
+							neighbours.get(particle1.getId()).add(particle2);
+						if (!neighbours.get(particle2.getId()).contains(particle1))
+							neighbours.get(particle2.getId()).add(particle1);
 					}
 				}
 			}
-			System.out.println();
 		}
+		return neighbours;
 	}
 }
