5b477d5d68f37e9aa6c14d7da3cd6946e06b488b
Diego Bruno <dbruno@itba.edu.ar>
2019-03-21 18:46:20 -0300

    Added options init

--- TP2/src/main/java/Main.java
+++ TP2/src/main/java/Main.java
@@ -3,6 +3,7 @@ import model.enums.Rules;
 public class Main {
 
     public static void main(String[] args) {
+    	Options options = new Options(args);
         Simulation.simulate(2, 4, Rules.STANDARD, false);
     }
 
