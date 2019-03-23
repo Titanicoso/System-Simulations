c3af96f4cd054a9b8a880a6dbb188d8b2aeacf54
Agustin Calatayud <acalatayud@itba.edu.ar>
2019-03-23 19:43:38 -0300

    Added Ants

--- TP2/src/main/java/model/enums/Rules.java
+++ TP2/src/main/java/model/enums/Rules.java
@@ -10,7 +10,7 @@ public enum Rules implements Rule {
     STANDARD {
 
         @Override
-        public boolean apply(State state, Cell cell) {
+        public boolean apply(State state, Cell cell, Cell previouslyModified) {
 
             final List<Cell> neighbours = getNeighbours(state, cell);
             int alive = 0;
@@ -34,7 +34,7 @@ public enum Rules implements Rule {
     SEEDS {
 
         @Override
-        public boolean apply(State state, Cell cell) {
+        public boolean apply(State state, Cell cell, Cell previouslyModified) {
 
             if(cell.isAlive()) {
                 return true;
@@ -53,6 +53,29 @@ public enum Rules implements Rule {
         @Override
         public List<Cell> getNeighbours(State state, Cell cell) {
             return state.getMooreNeighbours(cell, 1);
+        }
+    },
+
+    ANTS {
+
+        @Override
+        public boolean apply(State state, Cell cell, Cell previouslyModified) {
+
+            if(cell == previouslyModified)
+                return true;
+
+            if(previouslyModified.isAlive()) {
+                return cell.getX() == previouslyModified.getX() + 1 &&
+                        cell.getY() == previouslyModified.getY() && cell.getZ() == previouslyModified.getZ();
+            }
+
+            return cell.getX() == previouslyModified.getX() - 1 &&
+                    cell.getY() == previouslyModified.getY() && cell.getZ() == previouslyModified.getZ();
+        }
+
+        @Override
+        public List<Cell> getNeighbours(State state, Cell cell) {
+            return state.getVonNeumannNeighbours(cell, 1);
         }
     }
 }
